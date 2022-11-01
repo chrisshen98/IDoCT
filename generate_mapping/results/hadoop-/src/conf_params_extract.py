b66e7f3451c2c88a59f31a681b3c7c689a2b2c36
zixi2 <zixi2@fa22-cs527-110.cs.illinois.edu>
2022-11-01 18:56:54 -0500

    update hadoop- again 202211011856

--- generate_mapping/results/hadoop-/src/conf_params_extract.py
+++ generate_mapping/results/hadoop-/src/conf_params_extract.py
@@ -11,29 +11,37 @@ for file in files:
             path = temp.split("")[0]
             testClass = temp.split("")[1].split("1m")[1]
             tempClass = path+testClass
-            testClasses.append("\""+tempClass+"\",")
+            testClasses.append("\""+tempClass+"\"")
         elif "("+tempClass+")" in line:
             temp = line.split("("+tempClass+")")[0]
             testMethod = temp.split("] ")[0]
-            testMethods.append("\""+tempClass+"#"+testMethod+"\",")
+            testMethods.append("\""+tempClass+"#"+testMethod+"\"")
             
 print("\n"+"Test classes:"+"\n")
-toWrite = "{\n"
+toWrite = "{"
+first = True
 for item in testClasses:
-    toWrite += "\t"+item+"\n"
-    print(item)
-    print("\n")
-toWrite += "}"
+    if not first:
+        toWrite += ","
+    first = False
+    toWrite += "\n"+"\t"+item
+    # print(item)
+    # print("\n")
+toWrite += "\n}"
 f = open("../test_class_list.json", "w")
 f.write(toWrite)
 f.close()
 
 print("\n"+"Test methods:"+"\n")
-toWrite = "{\n"
+toWrite = "{"
+first = True
 for item in testMethods:
-    toWrite += "\t"+item+"\n"
-    print(item)
-toWrite += "}"
+    if not first:
+        toWrite += ","
+    first = False
+    toWrite += "\n"+"\t"+item
+    # print(item)
+toWrite += "\n}"
 f = open("../test_method_list.json", "w")
 f.write(toWrite)
 f.close()
